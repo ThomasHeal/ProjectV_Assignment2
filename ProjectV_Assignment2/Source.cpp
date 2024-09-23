7062b2ae3d5dadd67bda01b57a949adc4cfcc94f
Thomas heal <81531490+ThomasHeal@users.noreply.github.com>
2024-09-23 19:56:13 -0400

    Added Project Files
    - Added StudentData.txt to Resource Files
    - Added StudentData_Emails.txt to Resource Files
    - Updated files to "Content - Yes" in properties
    - Updated project to include content in OutDir

--- ProjectV_Assignment2/Source.cpp
+++ ProjectV_Assignment2/Source.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
+struct student_data {
+	string first_name;
+	string last_name;
+};
+
 int main() {
+
+	//read from the file and just write everythnig to console
+	ifstream file("StudentData.txt");
+	string line;
+	while (getline(file, line)) {
+		cout << line << endl;
+	}
+
+	//wait for user input
+	cout << "Press Enter to continue...";
+	cin.get();
+		
+
+
+
 	return 1;
 }
