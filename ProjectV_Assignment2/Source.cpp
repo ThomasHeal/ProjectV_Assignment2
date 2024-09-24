4a809ee4901f515c0c33ed02ec80cdf3a7304a91
Thomas heal <81531490+ThomasHeal@users.noreply.github.com>
2024-09-23 20:09:04 -0400

    Added Debug Print Statements
    - Added Debug Print Statements
    - Print student data if in debug mode

--- ProjectV_Assignment2/Source.cpp
+++ ProjectV_Assignment2/Source.cpp
@@ -35,5 +35,15 @@ int main() {
 		return 1;
 	}
 
+
+#ifdef _DEBUG
+	//print out the student list
+	for (int i = 0; i < student_list.size(); i++) {
+		cout << student_list[i].first_name << " " << student_list[i].last_name << endl;
+	}
+	cin.get();
+#endif
+
+
 	return 1;
 }
